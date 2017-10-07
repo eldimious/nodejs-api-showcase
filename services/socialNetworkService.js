1afa02f4bae811b26681b0cbfec44bf3bf482e71
Dimosthenis Botsaris <botsaris.d@gmail.com>
2017-10-07 13:44:22 +0300

    AddsocialNetworkService configuration

---
+++ services/socialNetworkService.js
@@ -0,0 +1,9 @@
+const debug = require('debug')('services:SOCIALNETWORK');
+
+
+function init(interfaces) {
+  debug('------- INIT SERVICES:SOCIALNETWORK ---------');
+
+}
+
+module.exports.init = init;

d92edd282062bfddf54c9db0fbe294ce96165ba4
Dimosthenis Botsaris <botsaris.d@gmail.com>
2017-10-14 23:09:18 +0300

    update error routes

--- domain/routes/errors.js
+++ domain/routes/errors.js
@@ -2,44 +2,74 @@ const debug = require('debug')('routes:errors');
 const errors = require('../../common/errors');
 
 function sendError(error, req, res, next) { //eslint-disable-line
-  debug('send error');
+  debug('send error', error);
+  const generic4xxError = /^[4][0-9][0-9]$/;
   if (error.constructor === errors.invalid_argument) {
     res.status(400).send({
-      status: error.message,
-      idx: error.idx,
+      status: 400,
+      data: {
+        message: error.message,
+        idx: error.idx,
+      },
     });
   } else if (error.constructor === errors.unauthorized) {
     res.status(401).send({
-      status: error.message,
+      status: 401,
+      data: {
+        message: error.message,
+      },
     });
   } else if (error.constructor === errors.not_modified) {
     res.status(304).send({
-      status: error.message,
+      status: 304,
+      data: {
+        message: error.message,
+      },
     });
   } else if (error.constructor === errors.forbidden) {
     res.status(403).send({
-      status: error.message,
+      status: 403,
+      data: {
+        message: error.message,
+      },
     });
   } else if (error.constructor === errors.duplicate) {
     res.status(409).send({
-      status: error.message,
+      status: 409,
+      data: {
+        message: error.message,
+      },
     });
-    // Include couchbase 'No such key' errors for 404
-  } else if (error.constructor === errors.not_found || error.code === 4104 || (error.code === 13)) {
+  } else if (error.constructor === errors.not_found) {
     res.status(404).send({
-      status: error.message,
-    });
-  } else if (error.code === 23) {
-    res.status(500).send({
-      status: error.message || 'Internal Error',
+      status: 404,
+      data: {
+        message: error.message,
+      },
     });
   } else if (error.constructor === errors.json_parse) {
     res.status(404).send({
-      status: error.message,
+      status: 404,
+      data: {
+        message: error.message,
+      },
+    });
+  } else if (generic4xxError.test(error.status)) {
+    res.status(error.status).send({
+      status: error.status,
+      data: {
+        message: error.message || 'Client Error',
+        stack: error.stack,
+      },
     });
   } else {
     res.status(500).send({
-      status: error.message || 'Internal Error',
+      status: 500,
+      data: {
+        message: 'Internal Server Error',
+        error: error.message,
+        stack: error.stack,
+      },
     });
   }
 }

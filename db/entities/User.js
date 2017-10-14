30aad04f610cb37ba04dddebdb1201bc79a3f4b4
Dimosthenis Botsaris <botsaris.d@gmail.com>
2017-10-14 22:59:53 +0300

    Add mongoose-paginate module

--- db/entities/User.js
+++ db/entities/User.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const mongoosePaginate = require('mongoose-paginate');
 const UserModel = require('../../models/User');
 
 module.exports = (mongoose) => {
@@ -21,6 +22,8 @@ module.exports = (mongoose) => {
   userSchema.statics.toUserModel = function(user) {
     return new UserModel(user.name, user.email, user.created, user['_id']);
   };
+
+  userSchema.plugin(mongoosePaginate);
 
   return mongoose.model('User', userSchema);
 };

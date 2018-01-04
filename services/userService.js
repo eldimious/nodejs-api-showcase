6325167def23cb33b8d6d8e5e8a6e8acb0d437ed
Dimosthenis Botsaris <botsaris.d@gmail.com>
2018-01-04 11:35:41 +0200

    update comments

--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,5 @@
 // DOMAIN LAYER
-// Has the userInterface as a dependency. The DriverService does not know
+// Has the userInterface as a dependency. The UserService does not know
 // nor does it care where the user models came from. This is abstracted away
 // by the implementation of the interfaces. It just calls the needed interfaces
 // gets the results and usually applies some business logic on them.

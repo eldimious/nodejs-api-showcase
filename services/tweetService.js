70fe247f98df06926559b1d58d027c74ee884dcd
DimosthenisBotsaris <botsaris.d@gmail.com>
2018-02-11 17:27:30 +0200

    Add comments

--- services/tweetService.js
+++ services/tweetService.js
@@ -3,13 +3,10 @@
 // nor does it care where the tweet models came from. This is abstracted away
 // by the implementation of the interfaces. It just calls the needed interfaces
 // gets the results and usually applies some business logic on them.
-
-// I am using factory function using object literal and prototype to pass methods on prototype chain
+// I am using a factory function (using object literal and prototype) to pass methods on prototype chain
 // With factory functions(closures) we can have data privacy.
 // I am not using just closures like on authService/authInterface in order to pass methods on prototype chain
-// (factory fuctions is an alternative way to prototypes without the need to use new keyword)
-// The main difference of just using closures and using closure with object literal and prototype is that
-// with second way we pass methods to prototype chain. Returns an empty object instead of an object with functions
+
 
 const debug = require('debug')('services:TWEET');
 

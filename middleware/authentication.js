0fddb6983f1efa2cc17891c87ce7bc216e6b9773
eldimious <botsaris.d@gmail.com>
2018-10-15 16:09:33 +0300

    Remove logs

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,17 +1,14 @@
 const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../configuration');
 const errors = require('../common/errors');
-console.log('asdasdasdasdas')
 
 const authenticationError = new errors.Unauthorized('Invalid user token', 'INVALID_TOKEN');
 
 module.exports = function(req, res, next) {
   let token = req.headers['authorization'];
-  console.log("mmmmmmmmm", token)
   if (token) {
     token = token.includes('Bearer') ? token.split(' ').pop() : token;
     jwt.verify(token, jwtSecret, (err, decoded) => {
-      console.log('errerrerr', err.message)
       if (err) {
         return res.status(401).send({
           status: authenticationError.status,

7ba385971c79f0839087a4226288873695485c57
eldimious <botsaris.d@gmail.com>
2018-11-08 15:47:39 +0200

    improve errors

--- common/errors/index.js
+++ common/errors/index.js
@@ -1,19 +1,6 @@
 const httpErrors = require('throw-http-errors');
 
-const isCustomError = (error) => {
-  if (httpErrors.includes(error.name)) {
-    return true;
-  }
-  return false;
-};
-
-const createErrorMessage = (error, defaultMsg) => error && error.message ? error.message : defaultMsg;
-
-
 module.exports = Object.assign(
   {},
   httpErrors,
-  {
-    isCustomError,
-  },
 );

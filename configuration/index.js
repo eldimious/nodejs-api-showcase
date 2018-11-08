20f7e655ca977efa5fc1dbbfae4959f27e822f9f
eldimious <botsaris.d@gmail.com>
2018-11-08 16:33:56 +0200

    improve routes

--- configuration/index.js
+++ configuration/index.js
@@ -1,4 +1,3 @@
-'use strict';
 /**
   * This module is used to collect all the configuration variables,
   * like the environment vars, in one place so they are not scattered all over the whole codebase
--- domain/routes/auth.js
+++ domain/routes/auth.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const debug = require('debug')('routes-auth');
 const EndpointValidator = require('../../middleware/endpointValidator');
 const asyncWrapper = require('../utils/asyncWrapper');
 
@@ -7,35 +6,23 @@ const endpointValidator = new EndpointValidator();
 const router = express.Router({ mergeParams: true });
 
 function init({ authService }) {
-  debug(' ---------- Init routes-AUTH --------- ');
-
-  async function registerNewUser(req, res, next) {
-    debug('register new user');
-    const options = {
+  router.post('/register', endpointValidator.requireValidUserBody, asyncWrapper(async (req, res) => {
+    const result = await authService.register({
       name: req.body.name,
       surname: req.body.surname,
       email: req.body.email,
       password: req.body.password,
-    };
-    const result = await authService.register(options);
+    });
     return res.jsend(result);
-  }
+  }));
 
-
-  async function loginUser(req, res, next) {
-    debug('login user');
-    const options = {
+  router.post('/login', endpointValidator.requireBodyParamsForLogin, asyncWrapper(async (req, res) => {
+    const result = await authService.login({
       email: req.body.email,
       password: req.body.password,
-    };
-    const result = await authService.login(options);
+    });
     return res.jsend(result);
-  }
-
-
-  router.post('/register', endpointValidator.requireValidUserBody, asyncWrapper(registerNewUser));
-
-  router.post('/login', endpointValidator.requireBodyParamsForLogin, asyncWrapper(loginUser));
+  }));
 
   return router;
 }

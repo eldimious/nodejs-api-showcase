35631810788846b646b8e293395b9e8d3a2dc288
eldimious <botsaris.d@gmail.com>
2018-11-21 09:57:57 +0200

    Add username in user schema

--- db/entities/User.js
+++ db/entities/User.js
@@ -14,6 +14,10 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  username: {
+    type: String,
+    required: true,
+  },
   password: {
     type: String,
     required: true,

dc95b7d4ff7b59c3e633cc5492ee4e7502d0bf8f
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-04-05 14:29:08 +0300

    remove unused folder

--- data/infrastructure/db/dataStores/index.js
+++
@@ -1,8 +0,0 @@
-const postStore = require('./postStore');
-const userStore = require('./userStore');
-
-
-module.exports.create = schemas => ({
-  postStore: postStore.create(schemas),
-  userStore: userStore.create(schemas),
-});

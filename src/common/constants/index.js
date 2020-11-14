4a60ee89d3f43ae143d1cd834efa805d386534e3
Dimosthenis Botsaris <botsaris.d@gmail.com>
2020-11-14 21:07:03 +0200

    add password complexity

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -2,7 +2,17 @@ const USER_TOKEN_EXPIRATION = 86400;
 
 const USER_ROLE = 'user';
 
+const PASSWORD_COMPLEXITY = {
+  min: 8,
+  max: 35,
+  lowerCase: 1,
+  upperCase: 1,
+  numeric: 1,
+  symbol: 1,
+};
+
 module.exports = {
   USER_TOKEN_EXPIRATION,
   USER_ROLE,
+  PASSWORD_COMPLEXITY,
 };

70d0b3a2cd856e22681ed2187a11c785b9582b63
eldimious <botsaris.d@gmail.com>
2022-02-09 21:51:33 +0200

    Add express jwt

--- src/presentation/http/app.ts
+++ src/presentation/http/app.ts
@@ -1,19 +1,19 @@
 import http from 'http';
-import express, { Response, NextFunction } from 'express';
+import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import helmet from 'helmet';
 import path from 'path';
-import { authenticateEndpoint } from './middleware/authentication';
+import jwt from 'express-jwt';
 import { authRouter } from './routes/auth/routes';
 import { usersRouter } from './routes/users/routes';
 import { errorHandler } from './routes/errors/routes';
-import { asyncWrapper } from './utils/asyncWrapper';
 import { IServices } from '../../common/interfaces/IServices';
-import { IExpressRequest } from '../../common/interfaces/IExpressRequest';
+import config from '../../configuration';
 
+const { jwtSecret } = config;
 const compress = compression();
 const app = express();
 app.disable('x-powered-by');
@@ -27,10 +27,14 @@ app.use(cors());
 export const appServerFactory = {
   init(services: IServices): http.Server {
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(jwt({
+      secret: jwtSecret as string,
+      algorithms: ['HS256'],
+    })
+      .unless({
+        path: ['/auth/register', '/auth/login'],
+      }));
     app.use('/auth', authRouter.init(services));
-    app.all('*', asyncWrapper(authenticateEndpoint()), (_req: IExpressRequest, _res: Response, next: NextFunction) => {
-      next();
-    });
     app.use('/users', usersRouter.init(services));
     app.use(errorHandler);
     return http.createServer(app);
